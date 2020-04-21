546e222614c128b875a8c456278e6ec286b5f3d7
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2020-04-21 12:57:19 +0200

    Initial commit

---
+++ app/templates_c/d840a3be5572f12a6f81d27cb31da078a97533f7_0.file.CalcView.tpl.php
@@ -0,0 +1,142 @@
+<?php
+/* Smarty version 3.1.30, created on 2020-04-21 12:33:49
+  from "D:\Xampp\htdocs\kalkulator5_1\app\CalcView.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5e9ecc0dd72c94_88716627',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'd840a3be5572f12a6f81d27cb31da078a97533f7' => 
+    array (
+      0 => 'D:\\Xampp\\htdocs\\kalkulator5_1\\app\\CalcView.tpl',
+      1 => 1587465227,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5e9ecc0dd72c94_88716627 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_7371259455e9ecc0dd5dbf8_82952864', 'footer');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_2369631595e9ecc0dd72732_77031982', 'content');
+$_smarty_tpl->inheritance->endChild();
+$_smarty_tpl->_subTemplateRender(($_smarty_tpl->tpl_vars['conf']->value->root_path).("/templates/main.tpl"), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, true);
+}
+/* {block 'footer'} */
+class Block_7371259455e9ecc0dd5dbf8_82952864 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+Thank You for using my (unfortunately outdated) cellar calculator! Work on improvement is ongoing.<?php
+}
+}
+/* {/block 'footer'} */
+/* {block 'content'} */
+class Block_2369631595e9ecc0dd72732_77031982 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<h2 class="content-head is-center">Cellar calculator</h2>
+
+<div class="pure-g">
+<div class="l-box-lrg pure-u-1 pure-u-med-2-5">
+	<form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/app/calc.php" method="post">
+		<fieldset>
+
+			<label for="x">Entered amount</label>
+			<input id="x" type="text" placeholder="enter Your amount here" name="x" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->x;?>
+">
+
+			<label for="op">Please choose Your cellar</label>
+					<select id="op" name="op">
+
+<?php if (isset($_smarty_tpl->tpl_vars['res']->value->op_name)) {?>
+<option value="<?php echo $_smarty_tpl->tpl_vars['form']->value->op;?>
+">Again: <?php echo $_smarty_tpl->tpl_vars['res']->value->op_name;?>
+</option>
+<option value="" disabled="true">---</option>
+<?php }?>
+						<option value="CHF">CHF</option>
+                                                <option value="Euro">Euro</option>
+                                                <option value="Pound">GBP</option>
+                                                <option value="Dollar">USD</option>
+					</select>
+					
+			<button type="submit" class="pure-button">Exchange</button>
+		</fieldset>
+	</form>
+</div>
+
+<div class="l-box-lrg pure-u-1 pure-u-med-3-5">
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isError()) {?>
+	<h4>Errors occured: </h4>
+	<ol class="err">
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getErrors(), 'err');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['err']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['err']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+<?php }?>
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isInfo()) {?>
+	<h4>Infos: </h4>
+	<ol class="inf">
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getInfos(), 'inf');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['inf']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['inf']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+<?php }?>
+
+<?php if (isset($_smarty_tpl->tpl_vars['res']->value->result)) {?>
+	<h4>Amount in PLN:</h4>
+	<p class="res">
+	<?php echo $_smarty_tpl->tpl_vars['res']->value->result;?>
+
+	</p>
+<?php }?>
+
+</div>
+</div>
+
+<?php
+}
+}
+/* {/block 'content'} */
+}
