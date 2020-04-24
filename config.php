964f1ee275e4d959bb2b5fd4a2c1891851ef1a2e
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2020-04-24 14:46:08 +0200

    Update index.php

--- config.php
+++ config.php
@@ -8,3 +8,7 @@ $conf->server_name = 'localhost:80';
 $conf->server_url = 'http://'.$conf->server_name;
 $conf->app_root = '/Kalkulator_Walutowy';
 $conf->app_url = $conf->server_url.$conf->app_root;
+
+$conf->action_root = $conf->app_root.'/app/ctrl.php?action=';
+$conf->action_url = $conf->server_url.$conf->action_root;
+?>
