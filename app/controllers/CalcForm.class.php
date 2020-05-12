0a39d94f6c6fd5cb364c5a17c26568ba62952ef4
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2020-05-12 19:33:21 +0200

    Delete CalcForm.class.php

--- app/controllers/CalcForm.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class CalcForm {
-	public $x;
-	public $currency;
-        public $CHF_Curr = 4.31;
-        public $Euro_Curr = 4.58;
-        public $Pound_Curr = 4.98;
-        public $Dollar_Curr = 4.21;
-} 
