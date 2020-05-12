0f41fb5a4eb1d9a46411afc320ee45452f99af9b
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2020-05-12 19:32:14 +0200

    Create CalcForm.class.php

---
+++ app/forms/CalcForm.class.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\forms;
+
+class CalcForm {
+	public $x;
+	public $currency;
+        public $CHF_Curr = 4.31;
+        public $Euro_Curr = 4.58;
+        public $Pound_Curr = 4.98;
+        public $Dollar_Curr = 4.21;
+} 
