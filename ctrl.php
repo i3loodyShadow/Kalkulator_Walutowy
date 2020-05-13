fee9b6f96ac28aa1ffa88561f21c8683af8c570a
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2020-05-13 14:58:42 +0200

    Update ctrl.php

--- ctrl.php
+++ ctrl.php
@@ -2,18 +2,30 @@
 require_once 'init.php';
 
 switch ($action) {
-	default :
+	default : // 'calcView'
+		include 'check.php';
 		$ctrl = new app\controllers\CalcCtrl();
 		$ctrl->generateView ();
 	break;
+	case 'login':
+		$ctrl = new app\controllers\LoginCtrl();
+		$ctrl->doLogin();
+	break;
 	case 'calcCompute' :
+		include 'check.php';
 		$ctrl = new app\controllers\CalcCtrl();
 		$ctrl->process ();
 	break;
-	case 'action1' :
-		print('hello');
+	case 'logout' :
+		include 'check.php';
+		$ctrl = new app\controllers\LoginCtrl();
+		$ctrl->doLogout();
 	break;
-	case 'action2' :
-		print('goodbye');
+	case 'action1' :
+		print('reakcja na akcję publiczną ....');
+	break;
+	case 'action2':
+		include 'check.php';
+		print('reakcja na akcję niepubliczną ....');
 	break;
 }
