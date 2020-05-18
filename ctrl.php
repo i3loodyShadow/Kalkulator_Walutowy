4dc6a46a8b83da5cdc7720e26c60bf9a18e67d7b
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2020-05-18 20:18:33 +0200

    Update ctrl.php

--- ctrl.php
+++ ctrl.php
@@ -1,15 +1,12 @@
 <?php
 require_once 'init.php';
 
-getConf()->login_action = 'login';
+getRouter()->setDefaultRoute('calcShow');
+getRouter()->setLoginRoute('login');
 
-switch ($action) {
-	default :
-		control('app\\controllers', 'CalcCtrl',		'generateView', ['user','admin']);
-	case 'login': 
-		control('app\\controllers', 'LoginCtrl',	'doLogin');
-	case 'calcCompute' : 
-		control(null, 'CalcCtrl',	'process',		['user','admin']);
-	case 'logout' : 
-		control(null, 'LoginCtrl',	'doLogout',		['user','admin']);
-}
+getRouter()->addRoute('calcShow',    'CalcCtrl',  ['user','admin']);
+getRouter()->addRoute('calcCompute', 'CalcCtrl',  ['user','admin']);
+getRouter()->addRoute('login',       'LoginCtrl');
+getRouter()->addRoute('logout',      'LoginCtrl', ['user','admin']);
+
+getRouter()->go();
