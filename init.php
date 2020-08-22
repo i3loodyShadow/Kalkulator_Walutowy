6e5828b70111d283ddaac772c10d813bf8e7bf59
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2020-08-22 15:28:38 +0200

    Update init.php

--- init.php
+++ init.php
@@ -1,79 +1,27 @@
 <?php
 
-/** 
- * Inicjalizacja już prawie kompletnego frameworka
- *
- * @author Przemysław Kudłacik
- */
-
 require_once 'core/Config.class.php';
-$conf = new core\Config();
-require_once 'config.php'; //ustaw konfigurację
+require_once 'core/App.class.php';
 
-function &getConf(){
-	global $conf; return $conf;
-}
+use core\App;
+use core\Config;
 
-require_once 'core/Messages.class.php'; //załaduj i stwórz Messages
-$msgs = new core\Messages();
-function &getMessages(){
-	global $msgs; return $msgs;
-}
+$_PARAMS = array(); #global array for parameters from clean URL
+$conf = new Config();
 
-require_once 'core/ClassLoader.class.php'; //załaduj i stwórz loader klas
-$cloader = new core\ClassLoader();
-function &getLoader() {
-    global $cloader; return $cloader;
-}
+# ---- Basic URL options - rather constant
+$conf->clean_urls = true;           # turn on pretty urls
+$conf->action_param = 'action';     # action parameter name (not needed for clean_urls)
+$conf->action_script = '/ctrl.php'; # front controller with location
 
-require_once 'core/Router.class.php'; //załaduj i stwórz router
-$router = new core\Router();
-function &getRouter() {
-    global $router; return $router;
-}
+include 'config.php'; //set user configuration
 
-$smarty = null;	//przygotuj Smarty, twórz tylko raz - wtedy kiedy potrzeba
-function &getSmarty(){
-	global $smarty;
-	if (!isset($smarty)){
-		//stwórz Smarty
-		include_once 'lib/smarty/Smarty.class.php';
-		$smarty = new Smarty();	
-		//przypisz konfigurację i messages
-		$smarty->assign('conf',getConf());
-		$smarty->assign('msgs',getMessages());
-		//zdefiniuj lokalizację widoków (aby nie podawać ścieżek przy odwoływaniu do nich)
-		$smarty->setTemplateDir(array(
-			'one' => getConf()->root_path.'/app/views',
-			'two' => getConf()->root_path.'/app/views/templates'
-		));
-	}
-	return $smarty;
-}
+# ---- Helpful values generated automatically
+$conf->root_path = dirname(__FILE__);
+$conf->server_url = $conf->protocol.'://'.$conf->server_name;
+$conf->app_url = $conf->server_url.$conf->app_root.$conf->public_dir;
+if ($conf->clean_urls) $conf->action_root = $conf->app_root."/"; #for clean urls
+else $conf->action_root = $conf->app_root.'/index.php?'.$conf->action_param.'='; #for regular urls
+$conf->action_url = $conf->server_url.$conf->action_root;
 
-$db = null; //przygotuj Medoo, twórz tylko raz - wtedy kiedy potrzeba
-function &getDB() {
-    global $conf, $db;
-    if (!isset($db)) {
-        require_once 'lib/medoo/Medoo.php';
-        $db = new \Medoo\Medoo([
-            'database_type' => &$conf->db_type,
-            'server' => &$conf->db_server,
-            'database_name' => &$conf->db_name,
-            'username' => &$conf->db_user,
-            'password' => &$conf->db_pass,
-            'charset' => &$conf->db_charset,
-            'port' => &$conf->db_port,
-            'prefix' => &$conf->db_prefix,
-            'option' => &$conf->db_option
-        ]);
-    }
-    return $db;
-}
-
-require_once 'core/functions.php';
-
-session_start(); //uruchom lub kontynuuj sesję
-$conf->roles = isset($_SESSION['_roles']) ? unserialize($_SESSION['_roles']) : array(); //wczytaj role
-
-$router->setAction( getFromRequest('action') );
+App::createAndInitialize($conf);
