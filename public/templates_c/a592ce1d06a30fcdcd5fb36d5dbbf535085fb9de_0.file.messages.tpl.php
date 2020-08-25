3c96b21931634163b26b5c74d1c54d9c6c979834
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2020-08-25 14:51:28 +0200

    Update a592ce1d06a30fcdcd5fb36d5dbbf535085fb9de_0.file.messages.tpl.php

--- public/templates_c/a592ce1d06a30fcdcd5fb36d5dbbf535085fb9de_0.file.messages.tpl.php
+++ public/templates_c/a592ce1d06a30fcdcd5fb36d5dbbf535085fb9de_0.file.messages.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2020-08-19 19:18:46
+/* Smarty version 3.1.30, created on 2020-08-22 16:34:11
   from "D:\Xampp\htdocs\amelia_test\app\views\templates\messages.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_5f3d5ef6e796a3_38731720',
+  'unifunc' => 'content_5f412ce3ee8b91_46670602',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'a592ce1d06a30fcdcd5fb36d5dbbf535085fb9de' => 
     array (
       0 => 'D:\\Xampp\\htdocs\\amelia_test\\app\\views\\templates\\messages.tpl',
-      1 => 1597857524,
+      1 => 1598106847,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5f3d5ef6e796a3_38731720 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5f412ce3ee8b91_46670602 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 
 <?php if ($_smarty_tpl->tpl_vars['msgs']->value->isMessage()) {?>
@@ -31,10 +31,8 @@ $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->t
 if ($_from !== null) {
 foreach ($_from as $_smarty_tpl->tpl_vars['msg']->value) {
 ?>
-	<li class="msg<?php if ($_smarty_tpl->tpl_vars['msg']->value->isError()) {?>Errors occured:<?php }
-if ($_smarty_tpl->tpl_vars['msg']->value->isWarning()) {?>warning<?php }
-if ($_smarty_tpl->tpl_vars['msg']->value->isInfo()) {?>Infos:<?php }?>"><p class="coms"><?php echo $_smarty_tpl->tpl_vars['msg']->value->text;?>
-</p></li>
+	<li class="msg <?php if ($_smarty_tpl->tpl_vars['msg']->value->isError()) {?>error<?php }?> <?php if ($_smarty_tpl->tpl_vars['msg']->value->isWarning()) {?>warning<?php }?> <?php if ($_smarty_tpl->tpl_vars['msg']->value->isInfo()) {?>info<?php }?>"><?php echo $_smarty_tpl->tpl_vars['msg']->value->text;?>
+</li>
 	<?php
 }
 }
